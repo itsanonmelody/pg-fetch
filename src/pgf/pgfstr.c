f11901392c66484eba5e0a347122e1f44a9b913f
CynusW <davidbryansunjaya@gmail.com>
2020-04-16 21:56:51 +0700

    Modified pgfstr - added more functions and a bit of modification

--- src/pgf/pgfstr.c
+++ src/pgf/pgfstr.c
@@ -1,17 +1,18 @@
 #include "pgf/pgfstr.h"
+#include "pgf/pgfmem.h"
 
-#include <stdlib.h>
+#include <string.h>
 
 // Create a string.
 const struct PGF_String* PGF_CreateString(const char* value)
 {
-    struct PGF_String* str = malloc(sizeof(struct PGF_String));
-    memset(str, 0, sizeof(str));
+    struct PGF_String* str = PGF_Malloc(sizeof(struct PGF_String));
+    memset(str, 0, sizeof(struct PGF_String));
 
     size_t size = strlen(value);
     str->size = size;
 
-    str->value = malloc(size+1);
+    str->value = PGF_Malloc(size+1);
     strncpy(str->value, value, size);
     str->value[size+1] = '\0';
 
@@ -21,8 +22,8 @@ const struct PGF_String* PGF_CreateString(const char* value)
 // Free a string.
 void PGF_FreeString(const struct PGF_String* str)
 {
-    free(str->value);
-    free(str);
+    PGF_Free(str->value, str->size + 1);
+    PGF_Free(str, sizeof(struct PGF_String));
 }
 
 // Compare two strings.
@@ -35,4 +36,38 @@ _Bool PGF_CompareString(const struct PGF_String* lhs, const struct PGF_String* r
 _Bool PGF_CompareStringC(const struct PGF_String* lhs, const char* rhs)
 {
     return (lhs->size == strlen(rhs)) && (strncmp(lhs->value, rhs, lhs->size) == 0);
+}
+
+// Check if a string starts with a substring.
+_Bool  PGF_StringStartsWith(const struct PGF_String* source, const struct PGF_String* substr)
+{
+    return (source->size >= substr->size) && (strncmp(source->value, substr->value, substr->size) == 0);
+}
+
+// Check if a string starts with a substring.
+_Bool  PGF_StringStartsWithC(const struct PGF_String* source, const char* substr)
+{
+    size_t size = strlen(substr);
+    return (source->size >= size) && (strncmp(source->value, substr, size) == 0);
+}
+
+// Transform a string to lower case.
+void PGF_StringToLower(struct PGF_String* str)
+{
+    for (size_t i = 0; i < str->size; ++i)
+    {
+        str->value[i] = tolower(str->value[i]);
+    }
+}
+
+// Transform a string to lower case.
+struct PGF_String* PGF_StringToLowerC(const char* str)
+{
+    struct PGF_String* res = PGF_CreateString(str);
+    for (size_t i = 0; i < res->size; ++i)
+    {
+        res->value[i] = tolower(res->value[i]);
+    }
+
+    return res;
 }
